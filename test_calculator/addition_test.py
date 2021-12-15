652a3b19918fde4ed6554942026b1367c9a513aa
sandeepajaladi <90530329+sandeepajaladi@users.noreply.github.com>
2021-12-14 23:37:58 -0500

    Update addition.py

--- test_calculator/addition_test.py
+++ test_calculator/addition_test.py
@@ -1,1 +1,10 @@
-Update
+"""Testing Addition"""
+from classes.Addition import addition
+
+    from calculator.main import Calculator
+
+    def test_calculator_add_result():
+        """testing calculator result is 0"""
+        calc = Calculator()
+        result = calc.add(1, 2)
+        assert result == 3
